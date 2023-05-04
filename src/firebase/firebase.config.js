0db6043e76135598d2a00b40831319fcfbd3b510
Hedayetul-Islam <hedayetul36@gmail.com>
2023-05-04 12:50:49 +0600

    tailwind & daisyui install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaOYqdH9Dap950jZ_I5tBnTGfFGjuo9as",
+  authDomain: "assignment10-client-project.firebaseapp.com",
+  projectId: "assignment10-client-project",
+  storageBucket: "assignment10-client-project.appspot.com",
+  messagingSenderId: "813040815951",
+  appId: "1:813040815951:web:5e490a0c0bca9fdc023f3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
