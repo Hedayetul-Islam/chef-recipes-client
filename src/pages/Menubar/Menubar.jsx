52693a942d7f757cc012d4213274e2c15113e584
Hedayetul-Islam <hedayetul36@gmail.com>
2023-05-05 20:04:06 +0600

    active route

--- src/pages/Menubar/Menubar.jsx
+++ src/pages/Menubar/Menubar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import ActiveRoute from '../ActiveRoute/ActiveRoute';
 
 const Menubar = () => {
     return (
         <div className="navbar bg-primary text-primary-content grid grid-cols-2">
             <a className="btn btn-ghost normal-case text-2xl">Best Chef Healthy Food</a>
            <div>
-           <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
-            <Link className="btn btn-ghost normal-case text-xl" to="/blog">Blog</Link>
-            <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
+           <ActiveRoute className="btn btn-ghost normal-case text-xl" to="/">Home</ActiveRoute>
+            <ActiveRoute className="btn btn-ghost normal-case text-xl" to="/blog">Blog</ActiveRoute>
+            <ActiveRoute className="btn btn-ghost normal-case text-xl" to="/login">Login</ActiveRoute>
            </div>
         </div>
     );
